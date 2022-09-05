63d7db88f31e62747127244e4d9298998eb83cf4
lucasrodriguesg <lucas.rodrigues.pvd@gmail.com>
2022-09-05 18:58:44 -0300

    Exercicios realizados

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+var sacolao = []
+
+const fruta1= {
+    nome: "Manga", 
+    preco: 4.99,
+    disponivel: true,
+}
+const fruta2= { 
+    nome: "Morango",
+    preco: 5.99,
+    disponivel: true,
+}
+const fruta3= {
+    nome: "Goiaba",
+    preco: 3.99,
+    disponivel: true
+}
+
+sacolao.push(fruta1, fruta2, fruta3)
+
+console.log(sacolao)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,12 @@ const filme = {
 };
 
 // respostas aqui ⬇️
+
+// Resposta A: Qual o código para imprimir o nome do **primeiro** ator/atriz?
+console.log("Nome do primeiro ator/atriz é: ", filme.elenco[0])
+// Resposta B: Qual o código para imprimir o nome do **último** ator/atriz?
+console.log("Nome do último ator/atriz é: ", filme.elenco[(filme.elenco.length-1)])
+//Resposta C: Qual o código para exibir o array com **todas** as transmissões de hoje?
+console.log("Segue os horarios de transmissão: ", filme.transmissoesHoje)
+//Resposta D: d) Qual o código para exibir o **horário** de transmissão do filme no `Canal Brasil`?
+console.log("No canal Brasil você pode assistir: ", filme.transmissoesHoje[1].horario)

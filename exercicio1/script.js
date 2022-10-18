dcac260b8c2b053dc69f1e51de3f2730c76bea58
lucasrodriguesg <lucas.rodrigues.pvd@gmail.com>
2022-10-18 15:50:15 -0300

    Exercicios do dia

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,10 @@
+//Crie um array vazio chamado `sacolao`
 var sacolao = []
 
+//Crie três objetos que vão representar frutas do sacolão.
+//nome: string;
+//preco: number;
+//disponibilidade: boolean
 const fruta1= {
     nome: "Manga", 
     preco: 4.99,
@@ -15,7 +20,8 @@ const fruta3= {
     preco: 3.99,
     disponivel: true
 }
-
+//Adicione os objetos ao array `sacolao` utilizando o método **`push()
 sacolao.push(fruta1, fruta2, fruta3)
 
+//Imprima a o array `sacolao`
 console.log(sacolao)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -3,19 +3,29 @@ const pokemon1 = {
 	tipo: "Grama",
 	nivel: 5
 };
-
+//Crie uma cópia do objeto, acima, mantendo a estrutura, mas trocando **`nome`** por “Squirtle” e **`tipo`** por “Água”
 const pokemon2 = {...pokemon1,
 nome: "Squirtle",
 tipo: "Água",
 }
-
+//No objeto original, adicione mais uma propriedade, chamada `ataques`.
 pokemon1.ataques = []
+
+//nome: Investida;
+//dano: 40;
+//tipo: Normal;
+//precisao: 100;
 pokemon1.ataques.push({nome: "Investida", dano: 40, tipo: "Normal", precisao: 100})
 
+//Na cópia do objeto original, adicione a propriedade **`ataques`** a partir do **espalhamento** da propriedade criada para o objeto original;
 pokemon2.ataques=[...pokemon1.ataques]
 
+//para o objeto original, adicione o ataque **“Folha Navalha”,** com **45 de dano**, **100 de precisão**, e de tipo **“Grama”**;
 pokemon1.ataques.push({nome: "Folha Lavalha", dano: 45, tipo: "Grama", precisao: 100})
+
+//para a cópia, crie o ataque **“Jato de Água”**, com **40 de dano**, **100 de precisão**, e do tipo **“Água”**
 pokemon2.ataques.push({nome: "Jato Água", dano: 40, tipo: "Água", precisao: 100})
 
+//Imprima os dois objetos no console
 console.log(pokemon1)
 console.log(pokemon2)
